64008ad89e571aa91faff919e0c4d94345010231
oya163 <oyeshsin@hotmail.com>
2016-03-24 17:28:50 -0500

    test changes

--- P2P.pro
+++ P2P.pro
@@ -13,8 +13,10 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+    lib.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h \
+    lib.h
 
 FORMS    += mainwindow.ui
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,25 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QFileDialog"
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_8_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"),
+                                                    "/home/oyashi/Downloads",
+                                                    tr("Torrents (*.torrent)"));
+    ui->fileLabel->setText(fileName);
 }
